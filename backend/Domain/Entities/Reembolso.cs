a5c2ef061a7d726c8ee9b9e4668923acee3f3c36
joaobrambati <joaobrambati@gmail.com>
2026-05-04 02:30:33 -0300

    feat: cria entidades e enum

---
+++ backend/Domain/Entities/Reembolso.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Reembolso
+{
+    public int Id { get; set; }
+    public TipoProcedimento TipoProcedimento { get; set; }
+    public DateTime DataProcedimento { get; set; }
+    public decimal ValorPago { get; set; }
+    public int ClienteId { get; set; }
+
+    public decimal ValorReembolsado { get; set; }
+    public StatusReembolso Status { get; set; }
+
+    public void DefinirComoRejeitado()
+    {
+        Status = StatusReembolso.Rejeitado;
+    }
+
+    public void DefinirComoAprovado()
+    {
+        Status = StatusReembolso.Aprovado;
+    }
+
+    public void DefinirComoFraude()
+    {
+        Status = StatusReembolso.SuspeitoDeFraude;
+    }
+
+    public void DefinirValorReembolso(decimal valor)
+    {
+        ValorReembolsado = valor;
+    }
+}
---
+++ backend/Domain/Enums/TipoProcedimento.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums;
+
+public enum TipoProcedimento
+{
+    ConsultaMedica,
+    ExameImagem,
+    ExameLaboratorial,
+    Outros
+}

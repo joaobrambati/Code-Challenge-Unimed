bd0b3e4821b358859b58484dbcbf83e6c35c18f5
joaobrambati <joaobrambati@gmail.com>
2026-05-04 02:33:58 -0300

    feat: configura DI, logging e referências entre projetos

--- backend/ReembolsoProcessor/Program.cs
+++ backend/ReembolsoProcessor/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Application.UseCases;
+using Infrastructure.Csv;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// 🔧 1. Configurar DI + Logging
+var services = new ServiceCollection();
+
+services.AddLogging(config =>
+{
+    config.AddConsole();
+});
+
+services.AddTransient<ProcessarReembolsosUseCase>();
+services.AddTransient<Application.Services.ReembolsoCalculator>();
+services.AddTransient<Application.Services.ReembolsoFraudChecker>();
+
+var provider = services.BuildServiceProvider();
+
+// INPUT
+var input = Console.In.ReadToEnd();
+
+// CSV → objetos
+var reader = new CsvInputReader();
+var pedidos = reader.Ler(input);
+
+// Processamento
+var useCase = provider.GetRequiredService<ProcessarReembolsosUseCase>();
+var resultado = useCase.Executar(pedidos);
+
+// objetos → CSV
+var writer = new CsvOutputWriter();
+var output = writer.Gerar(resultado);
+
+// OUTPUT
+Console.WriteLine(output);
